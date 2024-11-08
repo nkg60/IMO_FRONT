8d799b5f620295d154ec8162195d2a32019ad0c8
nkg60 <ngandeughisl@gmail.com>
2024-11-08 10:59:40 +0100

    initialisation du projet imo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogementListComponent } from './Features/logement-list/logement-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'logements', component: LogementListComponent },
+  { path: '', redirectTo: '/logements', pathMatch: 'full' }  // Redirige vers 'logements' par défaut
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; // Importer FormsModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogementListComponent } from './Features/logement-list/logement-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogementListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
